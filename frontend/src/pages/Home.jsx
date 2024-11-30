5caf25f512f128d189d616fe105a0e23c2c911a2
Ashish Kumar <ashishks1911@gmail.com>
2024-11-30 19:57:16 +0530

    front end home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import MainHeader from '../components/MainHeader'
+import HotelService from '../common/HotelService'
+import Parallax from '../common/Parallax'
 
 const Home = () => {
   return (
-    <div>
-      <h2>Welcome to the Home page</h2>
-    </div>
+    <section>
+      <MainHeader />
+      <section className='container'>
+        <Parallax />
+        <HotelService />
+        <Parallax />
+      </section>
+    </section>
   )
 }
 
