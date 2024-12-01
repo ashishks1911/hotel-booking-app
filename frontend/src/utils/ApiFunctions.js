3cf79cf6c991cad9b91fa1efd9e39392ae80985f
Ashish Kumar <ashishks1911@gmail.com>
2024-12-01 13:50:55 +0530

    added functions for bookings

--- frontend/src/utils/ApiFunctions.js
+++ frontend/src/utils/ApiFunctions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL : "http://localhost:9191"
+    baseURL: "http://localhost:9191"
 })
 
 //This function adds a new room to the database 
@@ -12,32 +12,32 @@ export async function addRoom(photo, roomType, roomPrice) {
     formData.append("roomPrice", roomPrice)
     console.log(formData);
     const response = await api.post("/rooms", formData);
-    if(response.status===201){
+    if (response.status === 201) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
 // This function fetches all room types from the database
 export async function getRoomTypes() {
-    try{
+    try {
         const response = await api.get("/rooms/types")
         return response.data;
-    }catch(error){
+    } catch (error) {
         throw new Error("Error Fetching room types");
     }
 }
 
 export async function getAllRooms() {
-    try{
+    try {
         const response = await api.get("/rooms");
         return response.data;
 
-    }catch(error){
+    } catch (error) {
         throw new Error("Error fetching rooms");
     }
-    
+
 }
 
 // This function deletes the room by id
@@ -65,10 +65,56 @@ export async function updateRoom(roomId, room) {
 }
 
 export async function getRoomById(roomId) {
-    try{
+    try {
         const response = await api.get(`/rooms/${roomId}`)
         return response.data
-    }catch(error){
+    } catch (error) {
         throw new Error(`Error getting room by id : ${roomId}`)
     }
+}
+
+export async function bookRoom(roomId, booking) {
+    try {
+        const response = await api.post(`/bookings/room/${roomId}`, booking);
+        return response.data
+    } catch (error) {
+        if (error.response && error.response.data) {
+            throw new Error(error.response.data)
+        } else {
+            throw new Error(`Error booking room : ${error.message}`)
+        }
+    }
+}
+
+export async function getAllBookings() {
+    try {
+        const response = await api.get(`/bookings`);
+        return response.data
+    } catch (error) {
+        throw new Error(`Error fetching bookings :  ${error.message}`)
+    }
+}
+
+export async function getBookingByConfirmationCode(confirmationCode) {
+    try {
+        const response = await api.get(`/bookings/confirmation/${confirmationCode}`)
+        return response.data
+    } catch (error) {
+        if (error.response && error.response.data) {
+            throw new Error(error.response.data)
+        } else {
+            throw new Error(`Error find booking : ${error.message}`)
+        }
+    }
+
+}
+
+export async function cancelBooking(bookingId) {
+    try {
+        const result = await api.delete(`/bookings/${bookingId}`)
+        return result.data
+    } catch (error) {
+        throw new Error(`Error cancelling booking : ${error.message}`)
+    }
+    
 }
