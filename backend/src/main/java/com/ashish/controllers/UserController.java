749866571fb91accf74166c1cb1b7c8a8fe4ca11
Ashish Kumar <ashishks1911@gmail.com>
2025-04-16 12:05:17 +0530

    Access control changes

--- backend/src/main/java/com/ashish/controllers/UserController.java
+++ backend/src/main/java/com/ashish/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.ashish.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,17 +16,18 @@ public class UserController {
     @Autowired
     private UserService userService;
     @GetMapping
+    @PreAuthorize("hasRole('ROLE_SUPER_ADMIN')")
     public ResponseEntity<List<UserDTO>> getAllUsers(){
         List<UserDTO> users =  userService.getAllUsers();
         return ResponseEntity.status(HttpStatus.OK).body(users);
     }
 
     @GetMapping("/{email}")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     public ResponseEntity<UserDTO> getUserByEmail(@PathVariable String email){
         UserDTO user = userService.getUserByEmail(email);
         return ResponseEntity.status(HttpStatus.OK).body(user);
     }
-
 
 
 }
--- backend/src/main/java/com/ashish/security/SecurityConfig.java
+++ backend/src/main/java/com/ashish/security/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -58,7 +57,11 @@ public class SecurityConfig {
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**", "/api-docs/**", "/swagger-ui/**").permitAll()
+                        .requestMatchers("/auth/**",
+                                "/rooms/**",
+                                "/bookings/**",
+                                "/api-docs/**",
+                                "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated());
         http.authenticationProvider(authenticationProvider());
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
